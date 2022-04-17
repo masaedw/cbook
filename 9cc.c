0a2ec435d3b0111f2bbf49b3ba0838d4ca4f59dd
Masayuki Muto <masa.edw@gmail.com>
2022-04-18 08:47:31 +0900

    mac os x

--- 9cc.c
+++ 9cc.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
-    return 1;
-  }
-
-  char *p = argv[1];
-
-  printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
-  printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
-
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
+    char *p = argv[1];
+
+    printf("\t.section\t__TEXT,__text,regular,pure_instructions\n");
+    printf("\t.build_version macos, 12, 0\tsdk_version 12, 3\n");
+    printf("\t.global\t_main\n");
+    printf("\t.p2align\t2\n");
+    printf("_main:\n");
+    printf("\tmov w0, #%ld\n", strtol(p, &p, 10));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("\tadd w0, w0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("\tadd w0, w0, #-%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
     }
 
-    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    return 1;
-  }
-
-  printf("  ret\n");
-  return 0;
+    printf("\tret\n");
+    return 0;
 }
