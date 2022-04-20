0fd99baceba94a2f0713a75a77e356521e82d06e
Masayuki Muto <masa.edw@gmail.com>
2022-04-20 22:50:28 +0900

    use spaces instead of tab

--- codegen.c
+++ codegen.c
@@ -5,59 +5,59 @@ void gen(Node *node)
 {
     if (node->kind == ND_NUM)
     {
-        printf("\tmov X0, #%d\n", node->val);
-        printf("\tstr X0, [SP, #-16]!\n");
+        printf("  mov X0, #%d\n", node->val);
+        printf("  str X0, [SP, #-16]!\n");
         return;
     }
 
     gen(node->lhs);
     gen(node->rhs);
 
-    printf("\tldr X0, [SP], #16\n"); // rhs
-    printf("\tldr X1, [SP], #16\n"); // lhs
+    printf("  ldr X0, [SP], #16\n"); // rhs
+    printf("  ldr X1, [SP], #16\n"); // lhs
 
     // X1 op X0
 
     switch (node->kind)
     {
     case ND_ADD:
-        printf("\tadd X0, X1, X0\n");
+        printf("  add X0, X1, X0\n");
         break;
     case ND_SUB:
-        printf("\tsub X0, X1, X0\n");
+        printf("  sub X0, X1, X0\n");
         break;
     case ND_MUL:
-        printf("\tmul X0, X1, X0\n");
+        printf("  mul X0, X1, X0\n");
         break;
     case ND_DIV:
-        printf("\tsdiv X0, X1, X0\n");
+        printf("  sdiv X0, X1, X0\n");
         break;
     case ND_LT:
-        printf("\tcmp X1, X0\n");
-        printf("\tmov X1, #1\n");
-        printf("\tmov X2, #0\n");
-        printf("\tcsel X0, X1, X2, LO\n");
+        printf("  cmp X1, X0\n");
+        printf("  mov X1, #1\n");
+        printf("  mov X2, #0\n");
+        printf("  csel X0, X1, X2, LO\n");
         break;
     case ND_LE:
-        printf("\tcmp X1, X0\n");
-        printf("\tmov X1, #1\n");
-        printf("\tmov X2, #0\n");
-        printf("\tcsel X0, X1, X2, LS\n");
+        printf("  cmp X1, X0\n");
+        printf("  mov X1, #1\n");
+        printf("  mov X2, #0\n");
+        printf("  csel X0, X1, X2, LS\n");
         break;
     case ND_EQ:
-        printf("\tcmp X1, X0\n");
-        printf("\tmov X1, #1\n");
-        printf("\tmov X2, #0\n");
-        printf("\tcsel X0, X1, X2, EQ\n");
+        printf("  cmp X1, X0\n");
+        printf("  mov X1, #1\n");
+        printf("  mov X2, #0\n");
+        printf("  csel X0, X1, X2, EQ\n");
         break;
     case ND_NE:
-        printf("\tcmp X1, X0\n");
-        printf("\tmov X1, #1\n");
-        printf("\tmov X2, #0\n");
-        printf("\tcsel X0, X1, X2, NE\n");
+        printf("  cmp X1, X0\n");
+        printf("  mov X1, #1\n");
+        printf("  mov X2, #0\n");
+        printf("  csel X0, X1, X2, NE\n");
         break;
     case ND_NUM:; // 警告抑制
     }
 
-    printf("\tstr X0, [SP, #-16]!\n");
+    printf("  str X0, [SP, #-16]!\n");
 }
